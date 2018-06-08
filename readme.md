071a7c831bf9ea5a159f4147f0bb281171497d5b
dhjw <deanhjw@gmail.com>
2018-06-08 16:31:51 -0700

    Enhance

--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
-1. Clone the repo. `git clone https://github.com/dhjw/tpb-hideporn`
+Enhance The Pirate Bay by hiding porn and adding search links
+
+1. Clone the repo. `git clone https://github.com/dhjw/tpb-enhance`
 2. Browse to `chrome://extensions`
 3. Enable Developer Mode
-4. Load unpacked extension > Choose tpb-hideporn folder
+4. Load unpacked extension > Choose tpb-enhance folder
--- tpb.js
+++ tpb.js
@@ -5,9 +5,21 @@ function afterDOMLoaded(){
 	var x=document.getElementsByClassName('vertTh');
 	var cnt=0;
 	for(let i=0;i<x.length;i++){
+		// hide porn rows
 		if(x[i].innerHTML.indexOf('Porn')!==-1){
 			x[i].parentElement.style.display='none';
 			cnt++;
+		}
+		// add google search for movies
+		if(x[i].innerHTML.indexOf('Movies')!==-1){
+			var txt=x[i].nextSibling.nextSibling.children[0].children[0].innerHTML.replace(/[\W_]+/g,' ').replace(/\s+/,' ').trim()+' ';
+			var re=/\s[0-9]{4}\s/g;
+			var pos=-1;
+			while ((match = re.exec(txt)) != null) pos=match.index;
+			if(pos!==-1){
+				txt=txt.substr(0,pos+5).trim();
+				x[i].nextSibling.nextSibling.children[0].innerHTML+=' (<a target="_blank" class="detLink" href="https://google.com/search?q='+encodeURIComponent(txt+' site:imdb.com OR site:rottentomatoes.com OR site:wikipedia.com')+'">g</a>)';
+			}
 		}
 	}
 	// search option
